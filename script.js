4ad10302c925fa652ef4166822fdb1047af4db0f
Maaz66-9 <maazsiddiquemd@gmail.com>
2025-06-28 15:09:02 +0530

    First Game Upload - Bubble Game

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
-function Makebubbles () {
-let frist = "";
+let hitt = 0;
 
-for (let i = 0; i <=132; i++) {
-   
-    let a =  Math.floor(Math.random()*10)
-    
-    frist +=  `<div class="bubble">${a}</div>`
+function Makebubbles() {
+  let frist = "";
+
+  for (let i = 0; i <= 132; i++) {
+    let a = Math.floor(Math.random() * 10);
+
+    frist += `<div class="bubble">${a}</div>`;
+  }
+
+  document.querySelector("#pbotom").innerHTML = frist;
+}
+timer = 60;
+function run() {
+  let bob = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timex").textContent = timer;
+    } else {
+      clearInterval(bob);
+      document.querySelector("#pbotom").innerHTML = `<h2>Game Over</h2> <button id ="rtry"> Retry</button>` ;
+      document.querySelector("#rtry").addEventListener("click",function(){
+        location.reload()
+      })
+    }
+  }, 1000);
 }
 
-document.querySelector("#pbotom").innerHTML = frist;
+function hito() {
+  hitt = Math.floor(Math.random() * 10);
+  document.querySelector("#hit0").textContent = hitt;
 }
-let timer = 60
-function run()  {
-    let bob = setInterval (function(){
-        if (timer > 0){timer--;
-document.querySelector("#timex").textContent = timer;}
 
-  else {clearInterval(bob)};}, 1000)
-    
-
+let skre = 0;
+function skr() {
+  skre += 10;
+  document.querySelector("#skore").textContent = skre;
 }
-run()
-Makebubbles()
+
+document.querySelector("#pbotom").addEventListener("click", function (hit1) {
+  let clickno = Number(hit1.target.textContent);
+  if (clickno === hitt) {
+    skr();
+    Makebubbles();
+    hito();
+  }
+});
+
+run();
+Makebubbles();
+hito();
+
